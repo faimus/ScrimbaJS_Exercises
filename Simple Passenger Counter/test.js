19e9c803fe726920ec2719b9441c749b5ccae602
faimus <dependablefaisal@gmail.com>
2022-11-08 11:05:21 -0500

    Clean index.js and use test.js for testing code

---
+++ Simple Passenger Counter/test.js
@@ -0,0 +1,11 @@
+let count = 0
+
+console.log(count)
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
