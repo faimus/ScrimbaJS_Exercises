f20a9c1cd11945e61a7f5d1f756813848c727bea
faimus <dependablefaisal@gmail.com>
2022-11-09 11:53:46 -0500

    JS functions practice

--- Simple Passenger Counter/test.js
+++ Simple Passenger Counter/test.js
@@ -1,11 +1,39 @@
-let count = 0
+// let count = 0
 
-console.log(count)
+// console.log(count)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// function exercises:
+
+// function that logs a number 42
+function countdown() {
+  console.log(42)
+}
+countdown()
+
+// function that logs the sum of all lap times 
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+function logLapTIme(){
+  console.log(lap1+lap2+lap3)
+}
+logLapTIme()
+
+// Create a function that increments the lapsCompleted variable by 1
+// Run it three times 
+let lapsCompleted = 0
+function countLaps(){
+  lapsCompleted++
+}
+countLaps()
+countLaps()
+countLaps()
+console.log(lapsCompleted)
