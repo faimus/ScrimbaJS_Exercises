808bc9ee3816d7fbb2f9b6d4b864bfc859203ec5
faimus <dependablefaisal@gmail.com>
2022-11-19 22:13:26 -0500

    JS strings practice-2

--- Simple Passenger Counter/test.js
+++ Simple Passenger Counter/test.js
@@ -47,16 +47,6 @@ console.log(message+", "+username+"!")
 let messageToUser = message+", "+username+"!"
 console.log(messageToUser)
 
-// Create two variables, name and greeting. The name variable should store your name,
-// and the greeting should store e.g. "Hi, my name is "
-
-// Create a third variable, myGreeting, that contatenates the two strings
-// Log myGreeting to the console
-let name = "Faisal"
-let greeting = "Hi, my name is "
-let myGreeting = greeting + name
-console.log(myGreeting)
-
 // Output in comments. String + number in JS will always result in a win for the string
 console.log(4 + 5) // 9
 console.log("2" + "4") // "24"
@@ -73,3 +63,10 @@ let lastName = "Mustafa"
 let fullName = firstName + " " + lastName
 console.log(fullName)
 
+let name = "Linda"
+let greeting = "Hi there"
+// Create a function that logs out "Hi there, Linda!" when called
+function greet(){
+  console.log(greeting + ", " + name +"!")
+}
+greet()
