25753395c5b2f1ae6e402c64e9e438dff809edd8
faimus <dependablefaisal@gmail.com>
2022-11-20 02:07:30 -0500

    Practice part1 added (ecommerce examples)

---
+++ Practice Part 1/index.js
@@ -0,0 +1,8 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let errorEl = document.getElementById("error")
+function errorFunc(){
+    errorEl.textContent = "Something went wrong, please try again"
+}
